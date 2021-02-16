1cbdf728ea393dedcf60244d7d9ce55f7a580b5a
montohang <harmonsitohang17@gmail.com>
2021-02-16 12:40:46 +0700

    [MODIFIED soal1 and soal2]

--- soal1.js
+++ soal1.js
@@ -1,10 +1,43 @@
-function dictionary(firstWord, secondWord) {
-	let data = [ 'hot', 'dot', 'dog', 'lot', 'log' ];
+const dict = [ 'hot', 'dot', 'dog', 'lot', 'log' ];
+var arr = [];
 
-	let isDataFound = false;
-	let words = [];
-
-	for (let i = 0; i < data.length; i++) {}
+function matchHuruf(word, word1) {
+	let wordComparator = word;
+	for (let j = 0; j < word.length; j++) {
+		wordComparator = word;
+		for (let k = 97; k < 124; k++) {
+			var alphabet = String.fromCharCode(k);
+			wordComparator = wordComparator.split('');
+			wordComparator[j] = alphabet;
+			wordComparator = wordComparator.join('');
+			console.log(`is ${wordComparator} == ${word1}`);
+			if (wordComparator == word1) {
+				console.log(wordComparator);
+				return true;
+			}
+		}
+	}
 }
 
-dictionary('hot', 'dog');
+function printShortOfDict(word1, word2) {
+	for (let i = 0; i < dict.length; i++) {
+		if (i == 0 && dict[i] == word1) {
+			arr.push(word1);
+		} else if (i == 0) {
+			if (matchHuruf(dict[i], word1)) {
+				arr.push(word1);
+				arr.push(dict[i]);
+			}
+		} else if (dict[i] == word2) {
+			arr.push(word2);
+		} else if (matchHuruf(dict[i], word2)) {
+			arr.push(dict[i]);
+			arr.push(word2);
+			break;
+		} else if (dict.length - 1 == i && !matchHuruf(dict[i], word2)) {
+			arr = 'no way';
+		}
+	}
+	console.log(`ini dia kayaknya : ${arr}`);
+}
+printShortOfDict('hit', 'dog');
--- soal2.js
+++ soal2.js
@@ -1,37 +1,56 @@
-function word(word) {
-	let activeWord = word;
+const dictionary = [ 'pro', 'gram', 'merit', 'program', 'it', 'programmer' ];
 
-	let data = [ 'pro', 'gram', 'merit', 'program', 'it', 'programmer' ];
-	let isDataFound = false;
-	let words = [];
-
-	for (let i = 0; i < data.length; i++) {
-		if (activeWord.length > 0) {
-			if (activeWord.includes(data[i])) {
-				words.push(data[i]);
-				activeWord = activeWord.substring(data[i].length);
+const findWords = (words) => {
+	let done = false;
+	let outputWords = [];
+	while (!done) {
+		for (let i = 0; i < dictionary.length; i++) {
+			if (words.includes(dictionary[i])) {
+				done = true;
+				outputWords.push(dictionary[i]);
 			}
-		} else {
-			isDataFound = true;
-			activeWord = word;
-			console.log(`act ${activeWord}`);
-			for (let j = 0; j < data.length; j++) {
-				if (!words.includes(data[j]) && activeWord.length == word.length && activeWord.includes(data[j])) {
-					words.push(data[j]);
-					activeWord = activeWord.substring(data[j].length);
-				} else if (words.includes(data[j]) && activeWord.length < word.length) {
-					words.push(data[j]);
-					activeWord = activeWord.substring(data[j].length);
-				}
-			}
-			activeWord = activeWord.substring(data[i].length);
-		}
-		if (i == data.length - 1 && !isDataFound) {
-			console.log('no way');
-		} else if (i == data.length - 1) {
-			console.log(words);
 		}
 	}
-}
+	return outputWords;
+};
 
-word('programmerit');
+console.log(findWords('programmerit'));
+
+// function word(word) {
+// 	let activeWord = word;
+
+// 	let data = [ 'pro', 'gram', 'merit', 'program', 'it', 'programmer' ];
+// 	let isDataFound = false;
+// 	let words = [];
+
+// 	for (let i = 0; i < data.length; i++) {
+// 		if (activeWord.length > 0) {
+// 			if (activeWord.includes(data[i])) {
+// 				words.push(data[i]);
+// 				activeWord = activeWord.substring(data[i].length);
+// 			}
+// 		} else {
+// 			isDataFound = true;
+// 			activeWord = word;
+// 			console.log(`act ${activeWord}`);
+// 			for (let j = 0; j < data.length; j++) {
+// 				if (!words.includes(data[j]) && activeWord.length == word.length && activeWord.includes(data[j])) {
+// 					words.push(data[j]);
+// 					activeWord = activeWord.substring(data[j].length);
+// 				}
+// 				// else if (words.includes(data[j]) && activeWord.length < word.length) {
+// 				// 	words.push(data[j]);
+// 				// 	activeWord = activeWord.substring(data[j].length);
+// 				// }
+// 			}
+// 			activeWord = activeWord.substring(data[i].length);
+// 		}
+// 		if (i == data.length - 1 && !isDataFound) {
+// 			console.log('no way');
+// 		} else if (i == data.length - 1) {
+// 			console.log(words);
+// 		}
+// 	}
+// }
+
+// word('programmerit');
